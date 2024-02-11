53404d6395bd176c634314105f43fbdb033aa387
SUHAS DESHPANDE <deshpandesuhas107@gmail.com>
2024-02-11 22:43:08 +0530

    Suhas

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = '4110cfbce6a7b83da436cbe125b14335';
+const WEATHER_API_KEY = '';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
